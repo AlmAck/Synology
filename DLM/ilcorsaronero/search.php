5a4fec9d3b3f3231016ecdd7ddae501c0a623788
AlmAck <gluca86@gmail.com>
2025-06-15 00:14:18 +0200

    update search.php to parse new website

--- DLM/ilcorsaronero/search.php
+++ DLM/ilcorsaronero/search.php
@@ -4,160 +4,96 @@ class DLMSearchIlcorsaronero
 {
 
 
-    private $wurl = 'https://ilcorsaronero.info/';
-
-
-    private $qurl = 'argh.php?search=%s';
+    private $wurl = 'https://ilcorsaronero.link/';
+    private $qurl = 'search?q=%s';
 
     public function __construct()
     {
-	
         $this->qurl = $this->wurl . $this->qurl;
     }
 
     public function prepare($curl, $query)
     {
+        $url = sprintf($this->qurl, urlencode($query));
 
-        $url = $this->qurl;
-        curl_setopt($curl, CURLOPT_FAILONERROR, 1);
-        curl_setopt($curl, CURLOPT_REFERER, $url);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 20);
-        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en; rv:1.9.0.4) Gecko/2008102920 AdCentriaIM/1.7 Firefox/3.0.4');
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_URL, sprintf($url, urlencode($query)));
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_FAILONERROR => true,
+            CURLOPT_TIMEOUT => 20,
+            CURLOPT_REFERER => $this->wurl,
+            CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+            CURLOPT_SSL_VERIFYPEER => false,
+        ]);
     }
+
 
     public function parse($plugin, $response)
     {
+        libxml_use_internal_errors(true);
+        $dom = new DOMDocument();
+        @$dom->loadHTML($response);
+        $xpath = new DOMXPath($dom);
 
-        $pattern = '/(?<size>\d{1,3}\.\d.+(GB|MB))/';
-        preg_match_all($pattern, $response, $matchSize);
+        $rows = $xpath->query("//table//tbody/tr");
+        $res = 0;
 
-
-        $pattern = '/(?<date>\d{2}\.\d{2}.\d{2})/';
-        preg_match_all($pattern, $response, $matchDate);
-
-
-        $pattern = '/(?<page>HREF=".+?")/';
-        $matchNumber = preg_match_all($pattern, $response, $matchLinkPage);
-
-
-        $pattern = '/(?<seeds>#(\d{2}|C{2})C{2}(\d{2}|C{2})\'>(n|\d).*?(a|\d*<\/font>)<\/td>\s)/';
-        preg_match_all($pattern, $response, $matchSeed);
-
-
-        $pattern = '/(?<leechs>#(\d{4}|C{4})C{2}\'>(n|\d)(.|\d?)(a|\d?)\S+<\/td><\/TR>)/';
-        preg_match_all($pattern, $response, $matchLeech);
-
-
-        $pattern = '/value="(?<magnet>\w+)"/';
-        preg_match_all($pattern, $response, $matchMagnet);
-
-        $patternTitle = '/(?<id>\/\d{3,8})\/(?<title>.+)/';
-
-
-        for ($res = 0; $res < $matchNumber; $res++) {
-
-
-            foreach ($matchLinkPage['page'] as $key => $value) {
-
-                $parse[$key]["page"] = substr($value, 6, -1);
-
-
-                preg_match($patternTitle, $parse[$key]["page"], $matchTitle);
-                $parse[$key]["title"] = $matchTitle['title'];
-
+        foreach ($rows as $row) {
+            $titleNode = $xpath->query(".//th//a", $row)->item(0);
+            if (!$titleNode) {
+                echo "[WARN] No title found, skipping row\n";
+                continue;
             }
 
-            foreach ($matchSize['size'] as $key => $value) {
-
-                $parse[$key]["size"] = $value;
+            $cells = $xpath->query(".//td", $row);
+            if ($cells->length < 6) {
+                echo "[WARN] Unexpected number of <td>, got {$cells->length}, skipping row\n";
+                continue;
             }
 
+            $category = trim($cells->item(0)->nodeValue);
+            $title = trim($titleNode->nodeValue);
+            $page = 'https://ilcorsaronero.link' . $titleNode->getAttribute('href');
+            $seeds = (int)trim($cells->item(1)->nodeValue);
+            $leechs = (int)trim($cells->item(2)->nodeValue);
+            $sizeText = trim($cells->item(3)->nodeValue);
+            $datetime = trim($cells->item(4)->nodeValue);
 
-            foreach ($matchDate['date'] as $key => $value) {
-                $parse[$key]["datetime"] = $value;
+            // Convert size to bytes
+            if (stripos($sizeText, 'GiB') !== false) {
+                $size = floatval($sizeText) * 1024 * 1024 * 1024;
+            } elseif (stripos($sizeText, 'MiB') !== false) {
+                $size = floatval($sizeText) * 1024 * 1024;
+            } else {
+                $size = 0;
             }
 
-
-            foreach ($matchSeed['seeds'] as $key => $value) {
-
-                if ($value[9] != "n") {
-                    $parse[$key]["seeds"] = substr($value, 9, -13);
-                } else {
-                    $parse[$key]["seeds"] = '0';
-                }
-            }
-
-
-            foreach ($matchLeech['leechs'] as $key => $value) {
-
-                if ($value[9] != "n") {
-                    $parse[$key]["leechs"] = substr($value, 9, -17);
-                } else {
-                    $parse[$key]["leechs"] = '0';
-                }
-            }
-
-
-            foreach ($matchMagnet['magnet'] as $key => $value) {
-                $parse[$key]["download"] = "magnet:?xt=urn:btih:" . $value;
-
-            }
-
-            $page = $parse[$res]["page"];
-            $title = $parse[$res]["title"];
-            //$download= $parse[$res]["download"];
-            //$download=$page;
-
-            $curlDown = curl_init($page);
-            curl_setopt_array($curlDown, array(
+            // Fetch magnet link from detail page
+            $curl = curl_init($page);
+            curl_setopt_array($curl, [
                 CURLOPT_RETURNTRANSFER => 1,
                 CURLOPT_URL => $page,
-                CURLOPT_USERAGENT => 'Codular Sample cURL Request',
+                CURLOPT_USERAGENT => 'Mozilla/5.0',
                 CURLOPT_SSL_VERIFYPEER => false,
-            ));
-            $downPage = curl_exec($curlDown);
-            curl_close($curlDown);
+            ]);
+            $detailHtml = curl_exec($curl);
+            curl_close($curl);
 
-            preg_match_all("/href=\"(magnet:.+?)\"/", $downPage, $output_array);
-            $download = $output_array[1][0];
+            preg_match('/href="(magnet:\?xt=urn:btih:[^"]+)"/', $detailHtml, $magnetMatch);
+            $download = $magnetMatch[1] ?? '';
 
-
-            switch (substr($parse[$res]["size"], -2, -1)) {
-                case 'M':
-                    $parse[$res]["size"] = (float)$parse[$res]["size"] * 1024 * 1024;
-                    break;
-
-                default:
-                    $parse[$res]["size"] = (float)$parse[$res]["size"] * 1024 * 1024 * 1024;
-                    break;
-            }
-
-            $size = $parse[$res]["size"];
-
-
-            $year = '20' . substr($parse[$res]["datetime"], 6, 2);
-            $month = substr($parse[$res]["datetime"], 3, 2);
-            $day = substr($parse[$res]["datetime"], 0, 2);
-            $datetime = $year . "-" . $month . "-" . $day;
-
-
-            $seeds = (int)$parse[$res]["seeds"];
-
-            $leechs = ((int)$parse[$res]["leechs"]) - $seeds;
-
-            $category = 'DVDRIP';
-
+            // Normalize date to yyyy-mm-dd
+            $datetime = date('Y-m-d', strtotime($datetime));
             $hash = md5($title);
 
             $plugin->addResult($title, $download, $size, $datetime, $page, $hash, $seeds, $leechs, $category);
+            $res++;
         }
 
         return $res;
     }
+
 }
 
 ?>
